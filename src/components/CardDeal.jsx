f8749db411a6257281299f174560e347f09c419e
mahmoudmorad11 <107371764+mahmoudmorad11@users.noreply.github.com>
2025-03-25 17:52:20 -0400

    Update CardDeal.jsx

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,6 +1,30 @@
 import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2
+        className={`${styles.heading2} max-md:text-center max-md:justify-center max-md:items-center`}
+      >
+        Find a Better Card Deal in <br className="sm:block hidden" /> few easy
+        steps
+      </h2>
+      <p className={`${styles.paragraph} mt-10 max-w-[470px]`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+        deleniti. Alias veritatis pariatur atque corrupti tenetur quam illo quos
+        nemo!
+      </p>
+      <Button value="Get Started" styles="mt-10 rounded-[10px]" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img
+        src={card}
+        alt="Cards"
+        className="object-contain w-[100%] h-[100%]"
+      />
+    </div>
+  </section>
+);
 export default CardDeal;
